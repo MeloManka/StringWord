2cf0122890f048798c5e939b15d3416fd7507a65
MeloManka <MeloMankaXD@gmail.com>
2014-04-04 23:30:12 +0300

    words

--- Words.java
+++ Words.java
@@ -32,9 +32,51 @@ public class Words {
             }
     }
 
-    public void Show() //вывод на экран
+    public void Find() //Ищет совпадение слова из первого предложения с остальным текстом.
     {
-        System.out.print(text.get(0));
+        StringTokenizer sentence = new StringTokenizer(text.get(0)," ");
+        int value, value1 = 0;
+
+        while(sentence.hasMoreTokens())
+        {
+            String word = sentence.nextToken();
+            value = 0;
+            for(int i = 1; i < text.size(); ++i)
+            {
+                int num = Comparison(word,text.get(i));
+                if(num != 0)
+                {
+                    ++value;
+                }
+            }
+            if(value == (text.size()-1))
+            {
+                Show(word);
+                ++value1;
+            }
+
+        }
+        if(value1 == 0)
+            System.out.println("Such words are not.");
+
+    }
+    public int Comparison(String word,String sentence) //передаём одно слово и одну строку и ищем совпадение.
+    {
+        StringTokenizer sent = new StringTokenizer(sentence);
+        while(sent.hasMoreTokens())
+        {
+            String wordSent = sent.nextToken();
+            if(word.compareToIgnoreCase(wordSent) == 0)
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    public void Show(String word) //вывод на экран
+    {
+        System.out.println(word);
     }
 
 }
